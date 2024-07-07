256aa7979c9c3a15175c951ad9721ffcd3e83972
jdmzuluaica <testing@vortexbird.com>
2024-07-07 00:26:53 -0500

    ajustes del auth

--- finance-tracker/src/main/java/com/finance/tracker/service/AuthServiceImpl.java
+++ finance-tracker/src/main/java/com/finance/tracker/service/AuthServiceImpl.java
@@ -28,43 +28,47 @@ public class AuthServiceImpl implements AuthService {
     @Autowired
     UserRepository userRepository;
 
-
     @Override
-	@Transactional(readOnly = true)
-    public UserDTO login (UserDTO userDTO) throws NotFoundException {
+    @Transactional(readOnly = true)
+    public UserDTO login(UserDTO userDTO) throws NotFoundException {
         if (userDTO == null) {
-            throw new NotFoundException("null userDTO");           
+            throw new NotFoundException("null userDTO");
         }
 
-        if(userDTO.getEmail() == null || userDTO.getEmail().isBlank()) {
-            throw new NotFoundException("El email es requerido.");              
+        if (userDTO.getEmail() == null || userDTO.getEmail().isBlank()) {
+            throw new NotFoundException("El email es requerido.");
         }
 
-        if(userDTO.getPassword() == null || userDTO.getPassword().isBlank()) {
-            throw new NotFoundException("La password es requerida.");              
+        if (userDTO.getPassword() == null || userDTO.getPassword().isBlank()) {
+            throw new NotFoundException("La password es requerida.");
         }
 
         Optional<User> user = userRepository.findByEmail(userDTO.getEmail());
         if (!user.isPresent()) {
-            throw new NotFoundException("El email o la password son incorrectas.");              
+            throw new NotFoundException("El email o la password son incorrectas.");
         }
 
         User userEntity = user.get();
 
         Optional<String> passwordEncripted = CustomPasswordGenerator.hashPassword(userDTO.getPassword());
 
-		// se valida que la contraseña entrante se la misma a la asignada en bd
-		if (!passwordEncripted.isPresent() || !userEntity.getPassword().equals(passwordEncripted.get())) {
-			throw new NotFoundException("La password es incorrecta.");
+        // se valida que la contraseña entrante se la misma a la asignada en bd
+        if (!passwordEncripted.isPresent() || !userEntity.getPassword().equals(passwordEncripted.get())) {
+            throw new NotFoundException("La password es incorrecta.");
         }
 
         SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(CustomSecurityConstants.SUPER_SECRET_KEY));
-		String token = Jwts.builder().setIssuedAt(new Date()).setIssuer(CustomSecurityConstants.ISSUER_INFO)
-				.setSubject(userDTO.getEmail())
-				.setExpiration(new Date(System.currentTimeMillis() + CustomSecurityConstants.TOKEN_EXPIRATION_TIME))
-				.signWith(key).compact();             
-       
+        String token = Jwts.builder().setIssuedAt(new Date()).setIssuer(CustomSecurityConstants.ISSUER_INFO)
+                .setSubject(userDTO.getEmail())
+                .setExpiration(new Date(System.currentTimeMillis() + CustomSecurityConstants.TOKEN_EXPIRATION_TIME))
+                .signWith(key).compact();
+
+        userDTO.setUserId(userEntity.getUserId());
+        userDTO.setEmail(userEntity.getEmail());
+        userDTO.setUsername(userEntity.getUsername());
+        userDTO.setPassword(passwordEncripted.get());
         userDTO.setToken(token);
+        userDTO.setStatus(userEntity.getStatus());
         return userDTO;
     }
 
